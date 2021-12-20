bfe00cf337b6d72e7cb7d7038af7186a0f7c2ed9
Asaf <asaf.l@visual3d.co.il>
2021-12-20 11:49:08 +0200

    Stage 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useEffect, useCallback, useState } from "react";
+import hub from './Back_lines.png';
+
+
+
+
 
 function App() {
+
+  const refDiv = useRef();
+
+  const [scrollPos, setScrollPos] = useState({scrollX: 0, scrollY: 0});
+  const [mousePos, setMousePos] = useState({posX: 0, posY: 0});
+
+  
+
+  const handleScroll = (e) => {  //Temporary name!!
+    const x = e.scrollX;
+    const y = e.scrollY;
+    setScrollPos({...scrollPos ,scrollX: window.pageXOffset, scrollY: window.pageYOffset});
+    console.log(scrollPos)
+  }
+
+  const onMouseDown = (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    setMousePos({...mousePos, posX: x, posY: y})
+    console.log(mousePos)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    console.log(scrollPos);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+    <div ref={refDiv} onClick={onMouseDown} onScroll={handleScroll}>
+      <img src={hub}/>
     </div>
+
   );
 }
 
